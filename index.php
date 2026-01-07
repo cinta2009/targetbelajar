843d53b7ff8cdbb13cf81ab3bb6f332524296b87
cinta2009 <21cintatata@gmail.com>
2026-01-07 18:48:36 +0700

    warna3

--- index.php
+++ index.php
@@ -10,11 +10,11 @@
         display: flex;
         justify-content: center;
         padding-top: 50px;
-        background: #165ac0ff;
+        background: #81aae7ff;
     }
 
     .kalkulator {
-        background: #c51313ff;
+        background: #df7979ff;
         padding: 20px;
         border-radius: 10px;
         box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
