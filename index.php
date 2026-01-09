651dbbb54c4127f62193f33ef7cb661bb02f71d9
cinta2009 <21cintatata@gmail.com>
2026-01-09 18:42:53 +0700

    nam4

--- index.php
+++ index.php
@@ -167,7 +167,8 @@ if(isset($_GET['reset'])) {
                 <span style="font-weight:bold; margin-left:10px;">= ?</span>
             </div>
 
-            <button type="submit" name="hitung" class="btn-hitung">HITUNG TAKDIR SUPERMU BERAPA PERSEN🔥</button>
+            <button type="submit" name="hitung" class="btn-hitung">HITUNG TAKDIR SUPERMU BERAPA PERSEN
+                CINTAMUH🔥</button>
         </form>
 
         <?php
